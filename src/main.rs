445b48b546ff46fb92ab9b1d5dcb391634c524f1
oseias-romeiro <oseiasromeirom@gmail.com>
2023-01-22 18:28:41 -0300

    mover

--- src/main.rs
+++ src/main.rs
@@ -1,103 +1,13 @@
-struct Posicao{
-    x: u32,
-    y: u32,
-}
-impl Posicao {
-    fn new(x:u32, y:u32) -> Self {
-        Self {
-            x,
-            y,
-        }
-    }
-}
+mod xadrez;
 
-
-#[derive(Debug)]
-enum Cor {
-    Branco,
-    Preto,
-}
-
-#[derive(Debug)]
-enum Peca {
-    Cavalo(Cor),
-    Peao(Cor),
-    Dama(Cor),
-    Rei(Cor),
-    Torre(Cor),
-    Bispo(Cor),
-    Vazio,
-}
-
-impl Peca {
-    fn representacao_visual(&self)-> &str{
-        match self {
-            Peca::Torre(Cor::Preto) => "\u{2656}",
-            Peca::Cavalo(Cor::Preto) => "\u{2658}",
-            Peca::Bispo(Cor::Preto) => "\u{2657}",
-            Peca::Dama(Cor::Preto) => "\u{2655}",
-            Peca::Rei(Cor::Preto) => "\u{2654}",
-            Peca::Peao(Cor::Preto) => "\u{2659}",
-            Peca::Torre(Cor::Branco) => "\u{265C}",
-            Peca::Cavalo(Cor::Branco) => "\u{265E}",
-            Peca::Bispo(Cor::Branco) => "\u{265D}",
-            Peca::Dama(Cor::Branco) => "\u{265B}",
-            Peca::Rei(Cor::Branco) => "\u{265A}",
-            Peca::Peao(Cor::Branco) => "\u{265F}",
-            Peca::Vazio => " ",
-        }
-    }
-
-}
-
-struct Tabuleiro {
-    mapa: [[Peca;8];8],
-}
-
-
-impl Tabuleiro {
-    fn tabuleiro_inicial() -> Self {
-
-        Self {  
-            mapa: [
-                [Peca::Torre(Cor::Preto), Peca::Cavalo(Cor::Preto), Peca:: Bispo(Cor::Preto), Peca::Dama(Cor::Preto), Peca::Rei(Cor::Preto), 
-                 Peca:: Bispo(Cor::Preto), Peca::Cavalo(Cor::Preto), Peca::Torre(Cor::Preto)],
-                [Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto),
-                 Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto)],
-
-                [Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio],
-                [Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio],
-                [Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio],
-                [Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio],
-                
-                [Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco),
-                 Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco)],
-
-                [Peca::Torre(Cor::Branco), Peca::Cavalo(Cor::Branco), Peca:: Bispo(Cor::Branco), Peca::Dama(Cor::Branco),
-                 Peca::Rei(Cor::Branco), Peca:: Bispo(Cor::Branco), Peca::Cavalo(Cor::Branco), Peca::Torre(Cor::Branco)],
-            ],
-        }
-    }
-
-    fn imprime_tabuleiro(&self) {
-        for linha in &self.mapa {
-            println!("-----------------");
-            print!("|");
-            for peca in linha {
-                print!("{}", peca.representacao_visual());
-
-                print!("|");
-            }
-            println!();
-        }
-        println!("-----------------");
-
-    }
-}
-
-
+use xadrez::tabuleiro::Tabuleiro as Tabuleiro;
 
 fn main() {
+    // TODO: interação com o usuario
     let mut tab = Tabuleiro::tabuleiro_inicial();
+    tab.imprime_tabuleiro();
+    
+    // exemplo de movimento valido
+    tab.mover((1,0),(2,0));
     tab.imprime_tabuleiro();
 }
---
+++ src/xadrez/tabuleiro.rs
@@ -0,0 +1,123 @@
+struct Posicao{
+    x: u32,
+    y: u32,
+}
+impl Posicao {
+    fn new(x:u32, y:u32) -> Self {
+        Self {
+            x,
+            y,
+        }
+    }
+}
+
+
+#[derive(Debug, Clone, Copy)]
+enum Cor {
+    Branco,
+    Preto,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Peca {
+    Cavalo(Cor),
+    Peao(Cor),
+    Dama(Cor),
+    Rei(Cor),
+    Torre(Cor),
+    Bispo(Cor),
+    Vazio,
+}
+
+impl Peca {
+    fn representacao_visual(&self)-> &str{
+        match self {
+            Peca::Torre(Cor::Preto) => "\u{2656}",
+            Peca::Cavalo(Cor::Preto) => "\u{2658}",
+            Peca::Bispo(Cor::Preto) => "\u{2657}",
+            Peca::Dama(Cor::Preto) => "\u{2655}",
+            Peca::Rei(Cor::Preto) => "\u{2654}",
+            Peca::Peao(Cor::Preto) => "\u{2659}",
+            Peca::Torre(Cor::Branco) => "\u{265C}",
+            Peca::Cavalo(Cor::Branco) => "\u{265E}",
+            Peca::Bispo(Cor::Branco) => "\u{265D}",
+            Peca::Dama(Cor::Branco) => "\u{265B}",
+            Peca::Rei(Cor::Branco) => "\u{265A}",
+            Peca::Peao(Cor::Branco) => "\u{265F}",
+            Peca::Vazio => " ",
+        }
+    }
+
+}
+
+pub struct Tabuleiro {
+    mapa: [[Peca;8];8],
+}
+
+
+impl Tabuleiro {
+
+    pub fn tabuleiro_inicial() -> Self {
+
+        Self {  
+            mapa: [
+                [Peca::Torre(Cor::Preto), Peca::Cavalo(Cor::Preto), Peca:: Bispo(Cor::Preto), Peca::Dama(Cor::Preto), Peca::Rei(Cor::Preto), 
+                 Peca:: Bispo(Cor::Preto), Peca::Cavalo(Cor::Preto), Peca::Torre(Cor::Preto)],
+                [Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto),
+                 Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto), Peca::Peao(Cor::Preto)],
+
+                [Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio],
+                [Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio],
+                [Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio],
+                [Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio, Peca::Vazio],
+                
+                [Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco),
+                 Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco), Peca::Peao(Cor::Branco)],
+
+                [Peca::Torre(Cor::Branco), Peca::Cavalo(Cor::Branco), Peca:: Bispo(Cor::Branco), Peca::Dama(Cor::Branco),
+                 Peca::Rei(Cor::Branco), Peca:: Bispo(Cor::Branco), Peca::Cavalo(Cor::Branco), Peca::Torre(Cor::Branco)],
+            ],
+        }
+    }
+
+    pub fn imprime_tabuleiro(&self) {
+        for linha in &self.mapa {
+            println!("-----------------");
+            print!("|");
+            for peca in linha {
+                print!("{}", peca.representacao_visual());
+
+                print!("|");
+            }
+            println!();
+        }
+        println!("-----------------");
+
+    }
+
+    // TODO: definir as regras de movimento das outras pecas
+    fn valid_moves(&self, coord_peca:(usize, usize), _coord_vai:(usize, usize)) -> bool {
+
+        /* regras:
+         * impede movimentar uma peca vazia
+        */
+        if 
+            &self.mapa[coord_peca.0][coord_peca.1].representacao_visual() !=  &" "
+        {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    pub fn mover(&mut self, coord_peca:(usize, usize), _coord_vai:(usize, usize)) {
+
+        if self.valid_moves(coord_peca, _coord_vai) {
+            self.mapa[_coord_vai.0][_coord_vai.1] = self.mapa[coord_peca.0][coord_peca.1];
+            self.mapa[coord_peca.0][coord_peca.1] = Peca::Vazio;
+        }else {
+            println!("Movimento não aceito")
+        }
+    }
+    
+}
