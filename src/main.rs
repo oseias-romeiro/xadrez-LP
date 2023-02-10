19c60147c1dac5c5f74632442da614a4e3e64ef5
oseias-romeiro <oseiasromeirom@gmail.com>
2023-02-10 15:01:08 -0300

    interação pelo terminal

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,44 @@ mod xadrez;
 use xadrez::tabuleiro::Tabuleiro as Tabuleiro;
 
 fn main() {
-    // TODO: interação com o usuario
-    let mut tab = Tabuleiro::tabuleiro_inicial();
-    tab.imprime_tabuleiro();
-    
-    // exemplo de movimentos de peoes validos
-    tab.mover((1,0),(3,0));
-    tab.mover((6,1),(5,1));
 
-    tab.imprime_tabuleiro();
+    let mut tab = Tabuleiro::tabuleiro_inicial();
+
+    loop {
+        tab.imprime_tabuleiro();
+
+
+        // origemX,origemY -> destinoX,destinoY
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        input.pop();
+
+        if input.contains(" -> ") {
+            let coord = get_coord(input);
+
+            tab.mover(coord.0,coord.1);
+        }else {
+            println!("Entrada inválida");
+        }
+    }
+
+}
+
+fn get_coord(input:String) -> ((usize, usize), (usize, usize)) {
+
+    let coord: Vec<&str> = input.split(" -> ").collect();
+        
+    let coord_peca: Vec<&str> = coord[0].split(',').collect();
+    let coord_vai: Vec<&str> = coord[1].split(',').collect();
+
+    return (
+        (
+            coord_peca[0].to_string().parse().unwrap(),
+            coord_peca[1].to_string().parse().unwrap()
+        ),
+        (
+            coord_vai[0].to_string().parse().unwrap(),
+            coord_vai[1].to_string().parse().unwrap()
+        )
+    );
 }
